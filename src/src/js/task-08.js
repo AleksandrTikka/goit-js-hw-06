03ed684ac7e63d39b7f6d957a727f5f62fd18f38
AleksandrTikka <alexandertikka@gmail.com>
2022-05-29 19:10:03 +0300

    task 8 done

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,20 @@
 
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', onSubmitLogin);
+
+function onSubmitLogin(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value.trim() === "" || password.value.trim() === "") {
+        alert('Все поля должны быть заполнены')
+    } else {
+        let user = { email: '', password: '' };
+        user.email = email.value;
+        user.password = password.value;
+        console.log(user);
+        event.currentTarget.reset();
+    }
+}
