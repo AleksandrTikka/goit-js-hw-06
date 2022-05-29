33888dfc7742b9f0d44b70f48be6841238aac094
AleksandrTikka <alexandertikka@gmail.com>
2022-05-29 21:03:35 +0300

    task 9 done

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,17 @@
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+  console.log(getRandomHexColor());
+}  
+ 
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
